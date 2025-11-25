96c40f3d7b7db3f1645d24c749aae20e98738941
Abdulqadir75434 <aq987654325@gmail.com>
2025-11-25 22:38:21 +0500

    commit32

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const faqItems = document.querySelectorAll(".faq-item");
+// Hamburger menu for mobile nav
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+  const authLinks = document.querySelector(".auth");
 
-    faqItems.forEach(item => {
-        const header = item.querySelector(".faq-header");
-        header.addEventListener("click", () => {
-
-            faqItems.forEach(i => {
-                if (i !== item) {
-                    i.classList.remove("active");
-                }
-            });
-
-            item.classList.toggle("active");
-        });
+  if(menuToggle){
+    menuToggle.addEventListener("click", function() {
+      navLinks.classList.toggle("active");
+      authLinks.classList.toggle("active");
     });
+  }
+  
+  
+  document.querySelectorAll('.faq-header').forEach((header) => {
+    header.addEventListener('click', () => {
+      header.parentElement.classList.toggle('active');
+    });
+  });
+});
